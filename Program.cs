0265070f501716a940dd946fd0d5d5c830249f25
RD-Ricardo <ricardodias169@gmail.com>
2021-03-19 14:32:10 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 , cm , metro , km;
+            Console.WriteLine("Medidas");
+            Console.WriteLine("-------");
+            Console.Write("Entre com a medida (em metros):  ") ;
+            num1 = Convert.ToDouble(Console.ReadLine());
+            cm = num1 * 100;
+            Console.WriteLine($"{cm} cm ");
+            metro = num1 * 1;
+            Console.WriteLine($"{metro} m");
+            km = num1  * 0.001;
+            Console.WriteLine($"{km} km ");
+        }
+    }
+}
